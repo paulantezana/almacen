0d31f699ef60ec11e483c51e42f7c7c32d13c84d
paulantezana <contacto@paulantezana.com>
2018-11-05 07:39:06 -0500

    new updates

--- src/Controller/Admin/SucursalController.php
+++ src/Controller/Admin/SucursalController.php
@@ -1,36 +1,49 @@
 <?php
     namespace App\Controller\Admin;
 
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\Routing\Annotation\Route;
 
     use App\Entity\Sucursal;
+    use App\Form\SucursalType;
     
     class SucursalController extends AbstractController
     {
-        public function index()
+        public function index($currentPage = 1, $limit = 3)
         {
-            $entityManager = $this->getDoctrine()->getManager();
-            $sucursal = $entityManager->getRepository(Sucursal::class);
+            $repository = $this->getDoctrine()->getRepository(Sucursal::class);
+            $sucursales = $repository->findAll();
             return $this->render('admin/sucursal/index.html.twig',[
-                'sucursales' => $sucursal,
+                'sucursales' => $sucursales,
             ]);
         }
 
-        public function showForm(){
+        public function add(){
+            $sucursal = new Sucursal();
+            $form = $this->createForm(SucursalType::class, $sucursal);
+            return $this->render('admin/sucursal/add.html.twig',[
+                'sucursales' => '',
+                'form'=>$form->createView(),
+            ]);
+        }
+
+        public function edit(){
+            return $this->render('admin/sucursal/edit.html.twig',[
+                'sucursales' => '',
+            ]);
+        }
+
+        public function create(Request $request){
+            // return $this->red;
+        }
+
+        public function update(Request $request){
 
         }
 
-        public function create(){
-
-        }
-
-        public function update(){
-
-        }
-
-        public function delete(){
+        public function delete(Request $request){
 
         }
     }
